ccb4369d309e855e72c660e9d9340de1381d4778
nschatz27 <akschatz1s@capetigers.com>
2026-04-30 08:44:59 -0500

    begin from 32:24

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ weatherForm.addEventListener("submit", async event => {
 
 async function getWeatherData(city) {
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
     const response = await fetch(apiUrl);
 
@@ -50,13 +50,25 @@ function displayWeatherInfo(data) {
     card.style.display = "flex";
 
     const cityDisplay = document.createElement("h1");
-    const tempDisplay = document.createElement("h1");
-    const feelDisplay = document.createElement("h1");
-    const windDisplay = document.createElement("h1");
-    const humidityDisplay = document.createElement("h1");
-    const uvDisplay = document.createElement("h1");
-    const descDisplay = document.createElement("h1");
-    const weatherDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const feelDisplay = document.createElement("p");
+    const windDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const uvDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const weatherDisplay = document.createElement("p");
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${temp.toFixed(0)}°F`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidityDisplay");
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
 }
 
 function getWeatherEmoji(weatherId) {
